3050a1203247a88859100a9e4e137776d7103f38
21paradox <1036339815@qq.com>
2015-05-09 14:50:35 +0800

    change innterval

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,8 @@ module.exports = function (grunt) {
           throttled: 3,
           sauceConfig: {
             'video-upload-on-pass': false
-          }
+          },
+         'pollInterval': 20
         }
       }
     },
